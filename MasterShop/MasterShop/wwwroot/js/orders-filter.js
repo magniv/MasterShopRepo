94d944abe8ec7e02570c46c506fdeefb22af2fc3
fdsfdsf <gdfgsg>
2021-07-31 21:44:45 +0300

    admin page

---
+++ MasterShop/MasterShop/wwwroot/js/orders-filter.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    
+	$("#order_filter_btn").click(function () {
+		executeSearch();
+	});
+});
+
+function executeSearch() {
+	var name = $('#account_name_input').val();
+	var price = $('#price_input').val();
+	var date = $('#date_input').val();
+
+	$.ajax({
+		url: "/Orders/Filter",
+		dataType: "json",
+		type: "POST",
+		data: {
+			accountName: name,
+			price: price,
+			date: date
+		},
+		success: function (data) {
+			$('#search-results').tmpl(data).appendTo('#table-container');
+		},
+
+	});
+}
---
+++ MasterShop/MasterShop/wwwroot/js/search.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+	$("#product_search").on('keyup', function () {
+		var product_name = $(this).val().toLowerCase();
+		executeFilter($(this), product_name);
+	});
+	$("#account_search").on('keyup', function () {
+		var account_name = $(this).val().toLowerCase();
+		executeFilter($(this), account_name);
+	});
+	$("#category_search").on('keyup', function () {
+		var category_name = $(this).val().toLowerCase();
+		executeFilter($(this), category_name);
+	});
+	$("#order_search").on('keyup', function () {
+		var account_name = $(this).val().toLowerCase();
+		executeFilter($(this), account_name);
+	});
+});
+
+function executeFilter(element, name) {
+	var searchCol = element.closest('.container').find("#table-container tr");
+	searchCol.filter(function() {
+		$(this).toggle($(this).find('#search-column').text().toLowerCase().indexOf(name) > -1)
+	});
+	
+}
+
